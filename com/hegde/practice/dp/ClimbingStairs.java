97e9030dafb9a27381ae06456a899c48a537beb2
Subrahmanya-Hegde <subbu.v.h.234@gmail.com>
2024-05-21 23:14:53 +0530

    new : added number of ways nth stair can be climbed

---
+++ com/hegde/practice/dp/ClimbingStairs.java
@@ -0,0 +1,16 @@
+package com.hegde.practice.dp;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ClimbingStairs {
+
+    public static int climbStairs(int stair) {
+        List<Integer> steps = new ArrayList<>(Arrays.asList(1, 2));
+        for(int i = 2; i < stair; i++){
+            steps.add(steps.get(i - 1) + steps.get(i - 2));
+        }
+        return steps.get(stair - 1);
+    }
+}
