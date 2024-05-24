edd33ba7a500fc7e0559ebe2368e87a226454c00
Subrahmanya-Hegde <subbu.v.h.234@gmail.com>
2024-05-24 15:46:23 +0530

    new : min steps to reach bottom right

---
+++ com/hegde/practice/graph/MinSumToReachRightBottom.java
@@ -0,0 +1,31 @@
+package com.hegde.practice.graph;
+
+public class MinSumToReachRightBottom {
+
+    public static int getMinSumToReachBottomRight(int[][] grid){
+        int[] ans = new int[]{Integer.MAX_VALUE};
+        boolean[][] visited = new boolean[grid.length][grid[0].length];
+        traverse(grid, visited, ans, 0, 0, 0);
+        return ans[0];
+    }
+
+    private static void traverse(int[][] grid, boolean[][] visited, int[] ans, int row, int col, int curSum) {
+        int rows = grid.length;
+        int cols = grid[0].length;
+        if(row >= rows || col >= cols || row < 0 || col < 0 || visited[row][col]){
+            return;
+        }
+
+        curSum += grid[row][col];
+        if(row == rows - 1 && col == cols - 1){
+            ans[0] = Math.min(curSum, ans[0]);
+            return;
+        }
+
+        visited[row][col] = true;
+        traverse(grid, visited, ans, row + 1, col, curSum);
+        traverse(grid, visited, ans, row, col + 1, curSum);
+        traverse(grid, visited, ans, row - 1, col, curSum);
+        visited[row][col] = false;
+    }
+}
