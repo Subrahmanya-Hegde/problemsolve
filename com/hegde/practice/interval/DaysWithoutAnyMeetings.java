df8da2a40a90c4cebdc60ed108c21cc75ef389f5
Subrahmanya-Hegde <subbu.v.h.234@gmail.com>
2024-06-03 10:48:30 +0530

    new : days without any meetings

---
+++ com/hegde/practice/interval/DaysWithoutAnyMeetings.java
@@ -0,0 +1,29 @@
+package com.hegde.practice.interval;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class DaysWithoutAnyMeetings {
+
+    static Comparator<int[]> comparator = Comparator.comparingInt(o -> o[0]);
+    public static int countDays(int days, int[][] meetings) {
+        if (days == 1)
+            return 0;
+
+        Arrays.sort(meetings, comparator);
+
+        int nonMeetingDays = 0;
+        int leftMostEndingDay = meetings[0][1];
+        for (int i = 1; i < meetings.length; i++) {
+            if (meetings[i][0] > leftMostEndingDay) {
+                nonMeetingDays += (meetings[i][0] - leftMostEndingDay);
+            }
+            leftMostEndingDay = Math.max(leftMostEndingDay, meetings[i][1]);
+        }
+        if(leftMostEndingDay < days){
+            nonMeetingDays +=  (days - leftMostEndingDay);
+        }
+        return nonMeetingDays;
+    }
+
+}
