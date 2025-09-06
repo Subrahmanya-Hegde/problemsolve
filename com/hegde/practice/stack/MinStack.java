0fbf8280f4b4c7c06026bdd041a899cd8eba7cf6
Subrahmanya Hegde <subrahmanyahegde@hegde.local>
2025-09-06 19:17:47 +0530

    new | Min stack - https://leetcode.com/problems/find-minimum-in-rotated-sorted-array-ii/description/

---
+++ com/hegde/practice/stack/MinStack.java
@@ -0,0 +1,47 @@
+package com.hegde.practice.stack;
+
+import java.util.*;
+
+public class MinStack {
+
+    public static class Node{
+        int value;
+        int min;
+        public Node(int value, int min){
+            this.value = value;
+            this.min = min;
+        }
+    }
+
+    List<Node> stack;
+
+    public MinStack() {
+        stack = new Stack<>();
+    }
+
+    public void push(int value) {
+        int curMin = !stack.isEmpty() ? getMin() : value;
+        Node node = new Node(value, Math.min(curMin, value));
+        stack.add(node);
+    }
+
+    public void pop() {
+        stack.remove(lastIndex());
+    }
+
+    public int top() {
+        return stack.get(lastIndex()).value;
+    }
+
+    public int getMin() {
+        int lastIndex = lastIndex();
+        return stack.get(lastIndex).min;
+    }
+
+    private int lastIndex(){
+        int lastIndex = stack.size() - 1;
+        if(lastIndex < 0)
+            throw new IllegalArgumentException("Stack is empty");
+        return lastIndex;
+    }
+}
