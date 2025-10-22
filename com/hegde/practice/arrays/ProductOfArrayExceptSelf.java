5902bd1b4001161bf53f07dd981c877fbccdaf10
Subrahmanya Hegde <subbu.v.h.234@gmail.com>
2025-10-22 18:26:35 +0530

    new: Added product of array elements except self

---
+++ com/hegde/practice/arrays/ProductOfArrayExceptSelf.java
@@ -0,0 +1,26 @@
+package com.hegde.practice.arrays;
+
+public class ProductOfArrayExceptSelf {
+
+    public int[] productExceptSelf(int[] nums) {
+        int totalNumbers = nums.length;
+        int[] ans = new int[totalNumbers];
+
+        int[] leftProductArray = new int[totalNumbers];
+        int[] rightProductArray = new int[totalNumbers];
+        leftProductArray[0] = 1;
+        rightProductArray[0] = 1;
+        int leftProduct = 1, rightProduct = 1;
+        for(int i = 0; i < totalNumbers; i++){
+            leftProductArray[i] = leftProduct;
+            rightProductArray[totalNumbers - i - 1] = rightProduct;
+            leftProduct *= nums[i];
+            rightProduct *= nums[totalNumbers - i - 1];
+        }
+
+        for(int i = 0; i < totalNumbers; i++){
+            ans[i] = leftProductArray[i] * rightProductArray[i];
+        }
+        return ans;
+    }
+}
