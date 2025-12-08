a2fd77944a332a1f872461860d00d8d731f2a64a
Subrahmanya Hegde <subbu.v.h.234@gmail.com>
2025-12-08 21:35:48 +0530

    new: Print pyramid pattern

--- com/hegde/practice/pattern/Pyramid.java
+++ com/hegde/practice/pattern/Pyramid.java
@@ -3,18 +3,70 @@ package com.hegde.practice.pattern;
 public class Pyramid {
 
     public static void printPyramid(int depth){
-        for(int i = 0; i < depth; i++){
-            for(int j = 0; j < (depth - i - 1); j++){
-                System.out.print(" ");
+        for(var i = 0; i < depth; i++){
+            for(var j = 0; j < (depth - i - 1); j++){
+                printSpace();
             }
-            for(int j = 0; j < 2 * i + 1; j++){
-                System.out.print("*");
+            for(var j = 0; j < 2 * i + 1; j++){
+                printStar();
+            }
+            System.out.println();
+        }
+
+    }
+
+    public static void printReversePyramid(int depth){
+        for(var i = depth - 1; i >= 0; i--){
+            for(int j = 0; j < (depth - i - 1); j++){
+                printSpace();
+            }
+            for(var j = 0; j < 2 * i + 1; j++){
+                printStar();
+            }
+            for(var j = 0; j < (depth - i - 1); j++){
+                printSpace();
             }
             System.out.println();
         }
     }
 
-    public static void main(String[] args){
-        printPyramid(5);
+    public static void printRightTiltedPyramid(int n){
+        var depth = 2 * n;
+        for(var i = 1; i < depth; i++){
+            var starsToBePrinted = i > n ? depth - i : i;
+            for(var j = 0; j < starsToBePrinted; j++){
+                printStar();
+            }
+            System.out.println();
+        }
+    }
+
+    public static void printOneZeroHalfPyramid(int n){
+        var charToBePrinted = 1;
+        for(var i = 0; i < n; i++){
+            for(var j = 0; j <= i; j++){
+                System.out.print(charToBePrinted);
+                charToBePrinted ^= 1;
+            }
+            System.out.println();
+        }
+    }
+
+    public static void printHalfAlphabetsPyramid(int n){
+        var A = 97;
+        for(var i = 0; i < n; i++){
+            for(var j = 0; j <= i; j++){
+                System.out.print((char) (A + j));
+            }
+            System.out.println();
+        }
+    }
+
+    private static void printStar(){
+        System.out.print("*");
+    }
+
+    private static void printSpace(){
+        System.out.print(" ");
     }
 }
