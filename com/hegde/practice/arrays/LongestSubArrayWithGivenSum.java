7fbcba48f900186e6faa64665591b38051eb829f
Subrahmanya Hegde <subbu.v.h.234@gmail.com>
2025-12-11 21:57:42 +0530

    new: Longest sub-array with sum equals to target.

--- com/hegde/practice/arrays/LongestSubArrayWithGivenSum.java
+++ com/hegde/practice/arrays/LongestSubArrayWithGivenSum.java
@@ -1,5 +1,8 @@
 package com.hegde.practice.arrays;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class LongestSubArrayWithGivenSum {
     public static int longestSubArrayWithSumK(int[] array, int target){
         int length = array.length, answer = 0;
@@ -15,6 +18,24 @@ public class LongestSubArrayWithGivenSum {
             if(sum == target){
                 answer = Math.max(answer, (fastIndex - slowIndex + 1));
             }
+        }
+        return answer;
+    }
+
+    public static int longestSubArrayWithSumKWithNegative(int[] array, int target){
+        int length = array.length, answer = 0, prefixSum = 0;
+        Map<Integer,Integer> firstIndexOfSum = new HashMap<>();
+
+        for(int i = 0; i < length; i++){
+            prefixSum+= array[i];
+
+            int reminder = prefixSum - target;
+
+            if (firstIndexOfSum.containsKey(reminder)) {
+                int startIndex = firstIndexOfSum.get(reminder);
+                answer = Math.max(answer, i - startIndex);
+            }
+            firstIndexOfSum.putIfAbsent(prefixSum, i);
         }
         return answer;
     }
