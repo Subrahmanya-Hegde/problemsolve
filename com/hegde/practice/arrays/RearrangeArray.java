0555d5cfa23158713773603a48044ff275157b1f
Subrahmanya Hegde <subbu.v.h.234@gmail.com>
2025-12-15 22:33:23 +0530

    new: Rearrange array in +ve and -ve.

---
+++ com/hegde/practice/arrays/RearrangeArray.java
@@ -0,0 +1,28 @@
+package com.hegde.practice.arrays;
+
+public class RearrangeArray {
+
+    static {
+        for(int i = 0; i < 200; i++){
+            rearrangeArray(new int[]{});
+        }
+    }
+
+    public static int[] rearrangeArray(int[] nums) {
+        if(nums.length == 0)
+            return nums;
+
+        int[] result = new int[nums.length];
+        int positiveIndex = 0, negativeIndex = 1;
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] > 0){
+                result[positiveIndex] = nums[i];
+                positiveIndex += 2;
+            }else{
+                result[negativeIndex] = nums[i];
+                negativeIndex += 2;
+            }
+        }
+        return result;
+    }
+}
