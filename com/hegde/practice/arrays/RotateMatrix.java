071bcd290ded9ec6ce888f36d70e7b1c31f3cffb
Subrahmanya Hegde <subbu.v.h.234@gmail.com>
2025-12-18 22:46:58 +0530

    new: Rotate 2d array by 90 degree.

---
+++ com/hegde/practice/arrays/RotateMatrix.java
@@ -0,0 +1,28 @@
+package com.hegde.practice.arrays;
+
+public class RotateMatrix {
+
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                transpose(matrix, i, j);
+            }
+        }
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n / 2; j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][n - j - 1];
+                matrix[i][n - j - 1] = temp;
+            }
+        }
+    }
+
+    private static void transpose(int[][] matrix, int row, int column) {
+        int temp = matrix[row][column];
+        matrix[row][column] = matrix[column][row];
+        matrix[column][row] = temp;
+    }
+}
