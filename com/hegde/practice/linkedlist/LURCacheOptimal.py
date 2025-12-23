1dc5e48210ede58637ed8943364952fe20b9ef47
Subrahmanya Hegde <subbu.v.h.234@gmail.com>
2025-12-23 18:28:53 +0530

    new: LRUCache optimal approach in Python

---
+++ com/hegde/practice/linkedlist/LURCacheOptimal.py
@@ -0,0 +1,33 @@
+import collections
+
+from collections import deque
+
+class LRUCache:
+    def __init__(self, n):
+        self.capacity = n
+        self.queue = deque()
+        self.cache = {}
+
+
+    def get(self, key):
+        if key in self.cache:
+            self.queue.remove(key)
+            self.queue.append(key)
+            return self.cache[key]
+        else:
+            return None
+
+    def set(self, key, val):
+        if key in self.cache:
+            self.queue.remove(key)
+        elif len(self.queue) == self.capacity:
+            least_used_cache = self.queue.popleft()
+            del self.cache[least_used_cache]
+        self.cache[key] = val
+        self.queue.append(key)
+
+
+# debug your code below
+cache = LRUCache(2)
+cache.set('user1', 'Alex')
+print(cache.get('user1'))
