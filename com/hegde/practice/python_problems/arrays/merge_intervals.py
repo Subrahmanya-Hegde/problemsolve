7c25701a815cbeb46b1991bbe8dae692b869ec8e
Subrahmanya Hegde <subbu.v.h.234@gmail.com>
2025-12-26 16:29:38 +0530

    refactor: Merge intervals refactored.

--- com/hegde/practice/python_problems/arrays/merge_intervals.py
+++ com/hegde/practice/python_problems/arrays/merge_intervals.py
@@ -3,20 +3,11 @@ from typing import List
 
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        n = len(intervals)
-        if n == 1:
-            return intervals
-
-        ans = []
         intervals.sort(key=lambda x: x[0])
-
-        start, end = intervals[0]
-        for i in range(1, n):
-            if intervals[i][0] <= end:
-                end = max(end, intervals[i][1])
+        merged = []
+        for interval in intervals:
+            if not merged or merged[-1][1] < interval[0]:
+                merged.append(interval)
             else:
-                ans.append([start, end])
-                start, end = intervals[i]
-
-        ans.append([start, end])
-        return ans
+                merged[-1][1] = max(merged[-1][1], interval[1])
+        return merged
