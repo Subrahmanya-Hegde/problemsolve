3fc6735269045bd0ca51cfe8d433f25bbf71bd0d
Subrahmanya Hegde <subbu.v.h.234@gmail.com>
2025-12-28 15:38:06 +0530

    new: Range of a number in a sorted array.

---
+++ com/hegde/practice/python_problems/binary_search/search_range.py
@@ -0,0 +1,27 @@
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        left, right = 0, len(nums) - 1
+        result = [-1, -1]
+
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] == target:
+                result[0] = mid
+                right = mid - 1
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] == target:
+                result[1] = mid
+                left = mid + 1
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        return result
