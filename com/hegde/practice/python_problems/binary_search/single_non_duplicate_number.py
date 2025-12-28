360d299d40afde5ffb7b68fe11ef85a02784a712
Subrahmanya Hegde <subbu.v.h.234@gmail.com>
2025-12-28 22:27:19 +0530

    chore: Single non duplicate item in the array.

---
+++ com/hegde/practice/python_problems/binary_search/single_non_duplicate_number.py
@@ -0,0 +1,33 @@
+from typing import List
+
+
+"""
+
+https://leetcode.com/problems/single-element-in-a-sorted-array/
+O(nlogn) solution -
+Logic - pairs should appear on even odd indices. 
+So we need to find where it is broken.
+If nums[mid] == nums[mid + 1] then pairs are intact on the left. So look for right side.
+If mid % 2 == 1 make mid -= 1. Why? Because pairs always starts at even index. So if our logic should work then we need
+to compare the even and odd indices.
+
+O(n) solution -
+Just do the xor of all elements. And return the xor.
+"""
+class Solution:
+    def single_non_duplicate(self, nums: List[int]) -> int:
+
+        left, right = 0, len(nums) - 1
+
+        while left < right:
+            mid = (left + right) // 2
+
+            if mid % 2 == 1:
+                mid -= 1
+
+            if nums[mid] == nums[mid + 1]:
+                left = mid + 2
+            else:
+                right = mid
+
+        return nums[left]
