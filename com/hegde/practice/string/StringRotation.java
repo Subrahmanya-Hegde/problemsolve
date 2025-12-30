3024317d0dbbb28781cb246f6da208fa1273ca58
Subrahmanya Hegde <subbu.v.h.234@gmail.com>
2025-12-30 14:58:37 +0530

    fix: Updated the string rotation problem with KMP approach.

--- com/hegde/practice/string/StringRotation.java
+++ com/hegde/practice/string/StringRotation.java
@@ -3,7 +3,7 @@ package com.hegde.practice.string;
 public class StringRotation {
 
     public static void main(String[] args){
-        System.out.println(isRotationApproachTwo("mightandmagic",
+        System.out.println(isRotationApproachUsingKMP("mightandmagic",
                 "andmagicmight"));
     }
 
@@ -30,8 +30,7 @@ public class StringRotation {
     public static boolean isRotationApproachUsingKMP(String s1, String s2) {
         if (s1.length() != s2.length())
             return false;
-        String temp = s1 + s1;
-        return temp.contains(s2);
+        return containsPattern(s1 + s2, s2);
     }
 
     private static boolean containsPattern(String text, String pattern){
@@ -39,26 +38,46 @@ public class StringRotation {
         int patternLength = pattern.length();
 
         int[] longestPrefixSuffix = buildLongestPrefixSuffixTable(pattern);
-        return true;
+
+        int i = 0, j = 0;
+
+        while (i < textLength){
+            if (text.charAt(i) == pattern.charAt(j)){
+                i++;
+                j++;
+                if (j == patternLength)
+                    return true;
+            }else{
+                if (j != 0){
+                    j = longestPrefixSuffix[j - 1];
+                }else{
+                    i ++;
+                }
+            }
+        }
+        return false;
     }
 
+    /**
+     * We need to find the common prefix length from index 1.
+     */
     private static int[] buildLongestPrefixSuffixTable(String pattern) {
         int patternLength = pattern.length();
         int[] longestPrefixSuffix = new int[patternLength];
 
-        int prefixLength = 0;
+        int prefixIndex = 0;
         int patternIndex = 1;
 
         longestPrefixSuffix[0] = 0;
 
         while (patternIndex < patternLength) {
-            if (pattern.charAt(patternIndex) == pattern.charAt(prefixLength)) {
-                prefixLength++;
-                longestPrefixSuffix[patternIndex] = prefixLength;
+            if (pattern.charAt(patternIndex) == pattern.charAt(prefixIndex)) {
+                prefixIndex++;
+                longestPrefixSuffix[patternIndex] = prefixIndex;
                 patternIndex++;
             } else {
-                if (prefixLength != 0) {
-                    prefixLength = longestPrefixSuffix[prefixLength - 1];
+                if (prefixIndex != 0) {
+                    prefixIndex = longestPrefixSuffix[prefixIndex - 1];
                 }else {
                     longestPrefixSuffix[patternIndex] = 0;
                     patternIndex++;
