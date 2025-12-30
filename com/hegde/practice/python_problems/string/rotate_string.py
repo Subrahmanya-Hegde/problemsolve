e8351c3eee11212e53c18ef9a1a33a875e23f222
Subrahmanya Hegde <subbu.v.h.234@gmail.com>
2025-12-30 15:01:35 +0530

    new: rotate string in python.

---
+++ com/hegde/practice/python_problems/string/rotate_string.py
@@ -0,0 +1,6 @@
+"""
+https://leetcode.com/problems/rotate-string/description/
+"""
+class Solution:
+    def rotate_string(self, s: str, goal: str) -> bool:
+        return len(s) == len(goal) and goal in s + s
