d2452aa2230d44d3d03738ffc909721cc52d252b
Subrahmanya Hegde <subbu.v.h.234@gmail.com>
2026-01-05 22:36:26 +0530

    new: Cycle in the linked list

---
+++ practice/src/main/java/com/hegde/practice/linkedlist/CycleInLinkedList.java
@@ -0,0 +1,22 @@
+package com.hegde.practice.linkedlist;
+
+import com.hegde.practice.helper.ListNode;
+
+/**
+ * https://leetcode.com/problems/linked-list-cycle/
+ */
+public class CycleInLinkedList {
+    public boolean hasCycle(ListNode head) {
+        ListNode slow = head, fast = head;
+
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if(fast == slow){
+                return true;
+            }
+        }
+        return false;
+    }
+}
