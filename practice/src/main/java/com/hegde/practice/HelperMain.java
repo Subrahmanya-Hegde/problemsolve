16a95130817d1fea131005cd3204f1b6be7c0073
Subrahmanya Hegde <subbu.v.h.234@gmail.com>
2026-01-10 22:58:03 +0530

    chore: Minor bug fix

--- practice/src/main/java/com/hegde/practice/HelperMain.java
+++ practice/src/main/java/com/hegde/practice/HelperMain.java
@@ -1,18 +1,25 @@
 package com.hegde.practice;
 
-
-import com.hegde.practice.stack.MinStack;
+import com.hegde.practice.lld.SlidingWindowRateLimiting;
 
 public class HelperMain {
 
-    public static void main(String[] args) {
-        MinStack minStack = new MinStack();
-        minStack.push(-2);
-        minStack.push(0);
-        minStack.push(-3);
-        System.out.println(minStack.getMin());
-        minStack.pop();
-        System.out.println(minStack.top());
-        System.out.println(minStack.getMin());
+    public static void main(String[] args) throws InterruptedException {
+        SlidingWindowRateLimiting rateLimiter = new SlidingWindowRateLimiting(3, 1000);
+        for(int i = 0; i < 1000; i++){
+            if(rateLimiter.allow("foo")){
+                System.out.println("Foo Hit");
+            }else {
+                System.out.println("Foo miss");
+            }
+            if(rateLimiter.allow("bar")){
+                System.out.println("Bar hit");
+            }else {
+                System.out.println("Bar miss");
+            }
+            if(i % 100 == 0){
+                Thread.sleep(1000);
+            }
+        }
     }
 }
